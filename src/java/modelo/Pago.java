343e8b715cd06f7bcb43eee813181b39e4cf00db
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-02 17:29:57 -0700

    Agregar clase Pago

--- src/java/modelo/Pago.java
+++ src/java/modelo/Pago.java
@@ -1,13 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Diego
- */
+import java.time.LocalDate;
+
 public class Pago {
-    
+
+    private int id;
+    private int idInquilino;
+    private int mes;
+    private LocalDate fecha;
+    private int monto;
+
+    public Pago(int id, int idInquilino, int mes, LocalDate fecha, int monto) {
+        this.id = id;
+        this.idInquilino = idInquilino;
+        this.mes = mes;
+        this.fecha = fecha;
+        this.monto = monto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdInquilino() {
+        return idInquilino;
+    }
+
+    public void setIdInquilino(int idInquilino) {
+        this.idInquilino = idInquilino;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pago other = (Pago) obj;
+        return this.id == other.id;
+    }
+
 }
