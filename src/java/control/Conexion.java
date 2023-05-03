ee8b6f785fb9e6e72b9100f545ddcaf3bb25206c
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-02 17:35:56 -0700

    Actualizar nombre de variable Connection
    De con a connection

--- src/java/control/Conexion.java
+++ src/java/control/Conexion.java
@@ -13,19 +13,19 @@ public class Conexion {
     private String DATABASE = "sistema";
     private String CLASSNAME = "com.mysql.cj.jdbc.Driver";
     private String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
-    private Connection con;
+    private Connection connection;
 
     public Conexion() {
         try {
             Class.forName(CLASSNAME);
-            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch(ClassNotFoundException | SQLException e) {
             System.err.println("Error en: " + e);
         }
     }
     
     public Connection getConexion() {
-        return con;
+        return connection;
     }
 
 }
