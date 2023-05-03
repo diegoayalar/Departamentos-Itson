d276d6ffa12c9033c22244ccf7afeedfdb3292fa
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-02 22:29:04 -0700

    Remover imports innecesarios

--- src/java/servlet/AgregarDepartamento.java
+++ src/java/servlet/AgregarDepartamento.java
@@ -7,8 +7,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import modelo.Departamento;
 
 public class AgregarDepartamento extends HttpServlet {
