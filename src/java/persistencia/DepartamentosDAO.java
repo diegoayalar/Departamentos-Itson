a9ab08b125dc0c3f20adcfaf5a39d142375ed536
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-03 13:02:07 -0700

    Agregar métodos para actualizar y eliminar departamento

--- src/java/persistencia/DepartamentosDAO.java
+++ src/java/persistencia/DepartamentosDAO.java
@@ -19,6 +19,25 @@ public class DepartamentosDAO extends ConexionBD {
         }
     }
 
+    public void actualizar(Departamento departamento) throws SQLException {
+        String sql = "UPDATE departamentos SET id_departamento = ?, ubicacion = ?, precio = ? WHERE id_departamento = ?";
+        try ( PreparedStatement pst = getConexion().prepareStatement(sql)) {
+            pst.setString(1, departamento.getId());
+            pst.setString(2, departamento.getUbicacion());
+            pst.setInt(3, departamento.getPrecio());
+            pst.setString(4, departamento.getId());
+            pst.executeUpdate();
+        }
+    }
+
+    public void eliminar(String id) throws SQLException {
+        String sql = "DELETE FROM departamentos WHERE id_departamento = ?";
+        try ( PreparedStatement pst = getConexion().prepareStatement(sql)) {
+            pst.setString(1, id);
+            pst.executeUpdate();
+        }
+    }
+
     public Departamento consultar(String id) throws SQLException {
         Departamento departamento = null;
         String sql = "CALL selectDepartamento(?)";
