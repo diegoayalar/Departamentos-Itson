15cc8f7b4c5f3278b0361a7d642dadc614a2e2c6
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-03 12:58:32 -0700

    Agregar servlet para eliminar departamento

---
+++ src/java/servlet/EliminarDepartamento.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import control.DepartamentosCtrl;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+public class EliminarDepartamento extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String id = request.getParameter("id");
+
+        DepartamentosCtrl departamentosCtrl = new DepartamentosCtrl();
+        departamentosCtrl.eliminar(id);
+
+        response.sendRedirect("departamentos.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+}
