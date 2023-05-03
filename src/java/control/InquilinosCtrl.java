721ae1e39ccd38a2b82ea0ff1e3997677578b28d
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-03 13:48:18 -0700

    Agregar clase de control de inquilinos

---
+++ src/java/control/InquilinosCtrl.java
@@ -0,0 +1,27 @@
+package control;
+
+import java.sql.SQLException;
+import java.util.List;
+import modelo.Inquilino;
+import persistencia.InquilinosDAO;
+
+public class InquilinosCtrl {
+
+    private final InquilinosDAO inquilinosDAO = new InquilinosDAO();
+    
+    public void agregar(Inquilino inquilino) throws SQLException {
+        inquilinosDAO.agregar(inquilino);
+    }
+    
+    public void actualizar(Inquilino inquilino) throws SQLException {
+        inquilinosDAO.actualizar(inquilino);
+    }
+    
+    public void eliminar(String id) throws SQLException {
+        inquilinosDAO.eliminar(id);
+    }
+    
+    public List<Inquilino> consultarTodos() throws SQLException {
+        return inquilinosDAO.consultarTodos();
+    }
+}
