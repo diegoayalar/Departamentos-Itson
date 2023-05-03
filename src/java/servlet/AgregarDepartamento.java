f5044814a33c3df4cdc4bbd3e308260d3074b1c3
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-02 22:12:23 -0700

    Agregar catchs en AgregarDepartamento

--- src/java/servlet/AgregarDepartamento.java
+++ src/java/servlet/AgregarDepartamento.java
@@ -2,41 +2,50 @@ package servlet;
 
 import control.DepartamentosCtrl;
 import java.io.IOException;
-import java.math.BigDecimal;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.Departamento;
-
 
 public class AgregarDepartamento extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
-        
+
         String id = request.getParameter("id");
         String ubicacion = request.getParameter("ubicacion");
         int precio = Integer.parseInt(request.getParameter("precio"));
-        
+
         Departamento departamento = new Departamento(id, ubicacion, precio);
         DepartamentosCtrl departamentosCtrl = new DepartamentosCtrl();
         departamentosCtrl.agregar(departamento);
-        
+
         response.sendRedirect("departamentos.jsp");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 
 }
