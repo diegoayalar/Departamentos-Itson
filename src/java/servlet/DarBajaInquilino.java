0715c1357fd36479d994b863654a08ab29d46b27
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-04 00:55:54 -0700

    Agregar servlet DarBajaInquilino

---
+++ src/java/servlet/DarBajaInquilino.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import modelo.Inquilino;
+import control.InquilinosCtrl;
+
+public class DarBajaInquilino extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String id = request.getParameter("id");
+        
+        InquilinosCtrl inquilinosCtrl = new InquilinosCtrl();
+        
+        Inquilino inquilino = inquilinosCtrl.consultar(id);
+        inquilino.setEstado(false);
+        inquilinosCtrl.actualizar(inquilino);
+
+        response.sendRedirect("inquilinos.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+}
