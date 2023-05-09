a77541bb3bd6b21bced461dff185d0e7a865b20d
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-08 22:30:50 -0700

    Agregar servlet AgregarPago

---
+++ src/java/servlet/AgregarPago.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import control.PagosCtrl;
+import modelo.Pago;
+
+public class AgregarPago extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        int idInquilino = Integer.parseInt(request.getParameter("idInquilino"));
+        int mes = Integer.parseInt(request.getParameter("mes"));
+        String fecha = request.getParameter("fecha");
+        int monto = Integer.parseInt(request.getParameter("monto"));
+
+        Pago pago = new Pago(idInquilino, mes, LocalDate.parse(fecha), monto, true);
+        PagosCtrl pagosCtrl = new PagosCtrl();
+        pagosCtrl.agregar(pago);
+
+        response.sendRedirect("pagos.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+}
