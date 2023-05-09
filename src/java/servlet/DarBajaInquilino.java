9dbe28b491c80f599f7c52a7689f878ac737b610
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-08 22:31:16 -0700

    Remover import java.time.LocalDate

--- src/java/servlet/DarBajaInquilino.java
+++ src/java/servlet/DarBajaInquilino.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import modelo.Inquilino;
 import control.InquilinosCtrl;
 
