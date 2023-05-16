d50aa29e16dcf23f63a323dc596ebe6d0f07c1a7
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-05-16 14:16:17 -0700

    Agregar validaciones de eliminación de departamento

--- src/java/servlet/EliminarDepartamento.java
+++ src/java/servlet/EliminarDepartamento.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 public class EliminarDepartamento extends HttpServlet {
 
@@ -17,9 +18,24 @@ public class EliminarDepartamento extends HttpServlet {
         String id = request.getParameter("id");
 
         DepartamentosCtrl departamentosCtrl = new DepartamentosCtrl();
-        departamentosCtrl.eliminar(id);
+        try {
+            // Intentar eliminar el departamento
+            departamentosCtrl.eliminar(id);
 
-        response.sendRedirect("departamentos.jsp");
+            // Si el departamento se elimina correctamente, redirigir a la página de inicio
+            response.sendRedirect("departamentos.jsp");
+
+        } catch (SQLIntegrityConstraintViolationException e) {
+            // Si se produce una excepción de violación de integridad de clave externa,
+            // mostrar un mensaje de error y redirigir a la página de inicio
+            request.setAttribute("mensaje", "No se puede eliminar el departamento porque tiene referencias.");
+            response.sendRedirect("departamentos.jsp");
+
+        } catch (SQLException e) {
+            // Si se produce cualquier otra excepción de SQL, mostrar un mensaje de error y redirigir a la página de inicio
+            request.setAttribute("mensaje", "Error al eliminar el departamento: " + e.getMessage());
+            response.sendRedirect("departamentos.jsp");
+        }
     }
 
     @Override
